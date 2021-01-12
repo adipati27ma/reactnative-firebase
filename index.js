245f64b03a60b597807592a72f2010866bb675aa
adipati27ma <adipati.cs.samsung@gmail.com>
2021-01-13 00:17:49 +0700

    solve warning 'setting a timer'

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+// solve warning 'Setting a timer for a long period of time'
+import {LogBox} from 'react-native';
+import _ from 'lodash';
+
+LogBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = (message) => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+
 AppRegistry.registerComponent(appName, () => App);
